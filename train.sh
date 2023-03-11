21cc7492c7471f6caf7442b83a36331f2a410ce6
zachary-shah <123213526+zachary-shah@users.noreply.github.com>
2023-03-11 11:32:39 -0800

    update terminal commands

--- train.sh
+++ train.sh
@@ -5,4 +5,7 @@ cd mel-train
 conda env create -f envs/control_env.yml
 conda activate control
 python cnet_riff_add_control.py
-python cnet_riff_training.py --train_data_dir "train-data/" --batch_size 4 --accumulate_gradient_batches 1
+
+
+python cnet_riff_training.py --train_data_dir "train-data/" --max_steps 100000 --save_gpu_memory True
+python upload_checkpoints.py 4
