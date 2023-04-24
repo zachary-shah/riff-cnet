5a42bdee1a42c73c590cb04f996b2a4ad4789cf3
zachary-shah <123213526+zachary-shah@users.noreply.github.com>
2023-04-24 09:58:19 -0700

    updates for training script to support different control methods

--- cldm/logger.py
+++ cldm/logger.py
@@ -11,7 +11,7 @@ from pytorch_lightning.utilities.distributed import rank_zero_only
 class ImageLogger(Callback):
     def __init__(self, batch_frequency=2000, max_images=4, clamp=True, increase_log_steps=True,
                  rescale=True, disabled=False, log_on_batch_idx=False, log_first_step=False,
-                 log_images_kwargs=None):
+                 log_images_kwargs=None, save_dir_root="image_log"):
         super().__init__()
         self.rescale = rescale
         self.batch_freq = batch_frequency
@@ -20,13 +20,14 @@ class ImageLogger(Callback):
             self.log_steps = [self.batch_freq]
         self.clamp = clamp
         self.disabled = disabled
+        self.save_dir_root = save_dir_root
         self.log_on_batch_idx = log_on_batch_idx
         self.log_images_kwargs = log_images_kwargs if log_images_kwargs else {}
         self.log_first_step = log_first_step
 
     @rank_zero_only
     def log_local(self, save_dir, split, images, global_step, current_epoch, batch_idx):
-        root = os.path.join(save_dir, "image_log", split)
+        root = os.path.join(save_dir, self.save_dir_root, split)
         for k in images:
             grid = torchvision.utils.make_grid(images[k], nrow=4)
             if self.rescale:
--- cnet_riff_training.py
+++ cnet_riff_training.py
@@ -16,7 +16,7 @@ def main():
         "--train_data_dir",
         type=str,
         nargs="?",
-            ult="train-data/",
+        default="train-data/",
         help="directory to read training dataset from"
     )
     parser.add_argument(
@@ -51,7 +51,7 @@ def main():
         "--max_steps",
         type=int,
         nargs="?",
-        default=10000,
+        default=100000,
         help="Max number of steps for training."
     )
     parser.add_argument(
@@ -75,67 +75,92 @@ def main():
         default=False,
         help="True to limit control to only the middle layers of model."
     )
+    parser.add_argument(
+        "--control_method",
+        type=str,
+        nargs="?",
+        default="",
+        help="For denoting control method used, if desired, for file access and saving."
+    )
+    parser.add_argument(
+        "--max_train_time",
+        type=str,
+        nargs="?",
+        default="00:12:00:00",
+        help="max training time default is 12 hours (form is \"DD:HH:MM:SS\")"
+    )
+    parser.add_argument(
+        "--model_config_path",
+        type=str,
+        nargs="?",
+        default="./models/cldm_v15.yaml",
+        help="max training time default is 12 hours (form is \"DD:HH:MM:SS\")"
+    )
     args = parser.parse_args()
 
-    # Configs
+    # Unchangeable configs
     cntrl_riff_path = "./models/control_riffusion_ini.ckpt"
-    max_train_time = "00:12:00:00" # max training time is 6 hours (form is "DD:HH:MM:SS")
-    logger_freq = args.logger_freq
-    train_data_dir = args.train_data_dir
-    accumulate_gradient_batches = args.accumulate_gradient_batches
-    max_steps = args.max_steps
-    save_gpu_memory = args.save_gpu_memory
-    only_mid_control = args.only_mid_control
-    batch_size = args.batch_size
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = f"max_split_size_mb:{args.max_split_size}"
 
     # SD_LOCKED: default is True. but reccomend trying false for unique image types. but then lower LR to 2e-6
     if args.sd_locked:
-        sd_locked = True 
         learning_rate = 1e-5
     else:
-        sd_locked = False
         learning_rate = 2e-6
 
     # load dataset. Pytorch Lightning will automatically move it to GPUs.
-    model = create_model('./models/cldm_v15.yaml').cpu()
+    # make models folder if not already made
+    os.makedirs("models/", exist_ok=True)
+
+    # where final checkpoint will be saved to
+    if len(args.control_method) >= 1:
+        save_path = "riff-cnet-"+args.control_method+"-final.ckpt"
+    else:
+        save_path = "riff-cnet-final.ckpt"
+    final_ckpt_path = os.path.join("models/",save_path)
+
+    # create controlnet and load in riffusion weights
+    model = create_model(args.model_config_path).cpu()
     model.load_state_dict(load_state_dict(cntrl_riff_path, location='cpu'))
     model.learning_rate = learning_rate
-    model.sd_locked = sd_locked
-    model.only_mid_control = only_mid_control
+    model.sd_locked = args.sd_locked
+    model.only_mid_control = args.only_mid_control
 
     # changes to make to save gpu memory
-    if save_gpu_memory:
+    if args.save_gpu_memory:
         move_metrics_to_cpu = True # reccomended to decrease gpu load, but makes training slower
-        accumulate_gradient_batches = batch_size
-        batch_size = 1 # accumulate gradients rather than train in batches. also slows training
         precision = 16 # divide 32-precision by 1/2
     else:
         move_metrics_to_cpu = False
         precision = 32 # TODO: maybe change this to 32 if things are successful
 
     # load in dataset
-    dataset = CnetRiffDataset(train_data_dir)
-    dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
+    if len(args.control_method) >= 1:
+        dataset = CnetRiffDataset(args.train_data_dir, promptfile="prompt-"+args.control_method+".json")
+    else:
+        dataset = CnetRiffDataset(args.train_data_dir)
+    dataloader = DataLoader(dataset, num_workers=0, batch_size=args.batch_size, shuffle=True)
 
-    print(f"Number of epochs to train: {np.ceil(max_steps * batch_size / len(dataset))}")
+    print(f"Number of epochs to train: {np.ceil(args.max_steps * args.batch_size / len(dataset))}")
 
     # make logger and model trainer
-    logger = ImageLogger(batch_frequency=logger_freq)
+    logger = ImageLogger(batch_frequency=args.logger_freq, clamp=False, save_dir_root="image_log_"+args.control_method)
     trainer = pl.Trainer(gpus=1, 
                          precision=precision, 
                          callbacks=[logger], 
-                         accumulate_grad_batches=accumulate_gradient_batches, 
-                         max_steps=max_steps, 
+                         accumulate_grad_batches=args.accumulate_gradient_batches, 
+                         max_steps=args.max_steps, 
                          move_metrics_to_cpu=move_metrics_to_cpu,
-                         max_time=max_train_time)
+                         max_time=args.max_train_time)
 
     # Train!
     trainer.fit(model, dataloader)
-    print("Training complete. Models and logging saved to directory.")
+    print("Training complete. Saving final checkpoint...")
 
     # SAVE FINAL CHECKPOINT
-    trainer.save_checkpoint("final_checkpoint.ckpt")
+    trainer.save_checkpoint(final_ckpt_path)
+
+    print(f"Final checkpoint saved to {final_ckpt_path}!")
 
 
 if __name__ ==  '__main__':
